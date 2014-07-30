e966bf99e48a5bec23d0385ad90b18e7139a3238
Dan Witten <dancwitten@gmail.com>
2014-07-29 23:38:00 -0400

    Adding a test class for Dollar, and tests the multiplication "times" method

---
+++ tests/DollarTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class DollarTest extends PHPUnit_Framework_TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $five->times(2);
+        $this->assertEquals(10, $five->amount);
+    }
+}
